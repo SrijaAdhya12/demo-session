02b1804fefd72fbf65489512f77ad6aa00d72d43
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-19 15:47:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express'
 import session from 'express-session'
 import dotenv from 'dotenv'
@@ -13,31 +14,31 @@ app.get('/', (_, res) => res.send('Welcome to Express Server'))
 
 const allowedOrigin = process.env.PRODUCTION_URL
 app.use(
-	cors({
-		origin: allowedOrigin,
-		credentials: true
-	})
+        cors({
+                origin: allowedOrigin,
+                credentials: true
+        })
 )
 
 const MongoDBStore = connectMongo(session)
 const store = new MongoDBStore({
-	uri: process.env.MONGO_URI,
-	collection: 'sessions'
+        uri: process.env.MONGO_URI,
+        collection: 'session'
 })
 
 store.on('error', (err) => console.log(err))
 
 app.use(
-	session({
-		secret: process.env.SESSION_SECRET,
-		resave: false, // this option specifies whether to save the session to the store on every request
-		saveUninitialized: false, // option specifies whether to save uninitialized sessions
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 24 * 7,
-			httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
-		},
-		store: store,
-	})
+        session({
+                secret: process.env.SECRET,
+                resave: false,
+                saveUninitialized: true,
+                cookie: {
+                        secure: false,
+                        maxAge: 1000 * 60 * 60 * 24 * 7
+                },
+                store: store
+        })
 )
 
 const port = process.env.PORT || 5000
@@ -46,15 +47,15 @@ app.use('/users', UserRoutes)
 app.use('/auth', AuthRoutes)
 
 app.get('/auth/check-session', (req, res) => {
-	if (req.session.user) {
-		res.json({ user: req.session.user })
-	} else {
-		res.status(401).json({ message: 'Not authenticated' })
-	}
+        if (req.session.user) {
+                res.json({ user: req.session.user })
+        } else {
+                res.status(401).json({ message: 'Not authenticated' })
+        }
 })
 
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(console.log('Connected to MongoDB Database 🌐'))
-	.then(() => app.listen(port, () => console.log(`Server running on port: ${port} 🚀`)))
-	.catch((error) => console.log(error.message))
+        .connect(process.env.MONGO_URI)
+        .then(console.log('Connected to MongoDB Database 🌐'))
+        .then(() => app.listen(port, () => console.log(`Server running on port: ${port} 🚀`)))
+        .catch((error) => console.log(error.message))
